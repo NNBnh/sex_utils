8fda86bcde628f1931dc1cb21c6babea87b6ab16
NNB <nnbnh@protonmail.com>
2022-01-21 10:39:35 +0700

    new file:   .github/workflows/main.yml 	new file:   .gitignore 	new file:   .rubocop.yml 	new file:   CHANGELOG.md 	new file:   CODE_OF_CONDUCT.md 	new file:   Gemfile 	new file:   LICENSE.txt 	new file:   README.md 	new file:   Rakefile 	new file:   bin/console 	new file:   bin/setup 	new file:   lib/sex_utils.rb 	new file:   lib/sex_utils/version.rb 	new file:   sex_utils.gemspec 	new file:   test/sex_utils_test.rb 	new file:   test/test_helper.rb

---
+++ lib/sex_utils.rb
@@ -0,0 +1,8 @@
+# frozen_string_literal: true
+
+require_relative "sex_utils/version"
+
+module SexUtils
+  class Error < StandardError; end
+  # Your code goes here...
+end
---
+++ lib/sex_utils/version.rb
@@ -0,0 +1,5 @@
+# frozen_string_literal: true
+
+module SexUtils
+  VERSION = "0.1.0"
+end
