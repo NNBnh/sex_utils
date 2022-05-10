ee662a107f492e23bd30950f9430589436f337b8
NNB <nnbnh@protonmail.com>
2022-05-10 21:53:36 +0000

    modified:   lib/sex_utils.rb 	modified:   sex_utils.gemspec

--- lib/sex_utils.rb
+++ lib/sex_utils.rb
@@ -1,174 +1,163 @@
 # frozen_string_literal: true
 
 require "fileutils"
+require "tty-reader"
 require_relative "sex_utils/version"
 
 module SexUtils
   class Error < StandardError; end
 
+  class Selection
+    def initialize(...)
+      @selection = selection
+      @reader = TTY::Reader.new(...)
+      @reader.on(:keyctrl_c, :keyescape) { exit }
+    end
 
-  @selection = []
-  
-  def s(paths)
-    @selection = Array(paths).flatten.map { |path| File.realpath path }
-  end
-  
-  
-  def cmp(a, b)
-    a ||= @selection[0] if @selection.one?
-  
-    unless b
-      print "Compare to: "
-      b ||= gets # TODO
+    def get(paths)
+      @selection
     end
-  
-    FileUtils.compare_file a, b
-  end
-  
-  
-  def copy_entry(
-    src, dest, preserve = false, dereference_root = false,
-    remove_destination = false
-  )
-    unless src
-      scr = @selection
-      dest = Dir.pwd
-    elsif Array(src).one?
-      print "Copy to: "
-      dest ||= gets # TODO
+
+    def set(paths)
+      @selection = Array(paths).flatten.map { |path| File.realpath path }
     end
-  
-    FileUtils.copy_entry(
-      src, dest, preserve = preserve, dereference_root = dereference_root,
-      remove_destination = remove_destination
+
+    def empty(paths)
+      @selection = []
+    end
+
+
+    def copy_entry(
+      src, dest = nil, preserve = false, dereference_root = false,
+      remove_destination = false
     )
-  end
-  
-  def copy_file(src, dest, preserve = false, dereference = true)
-    unless src
-      scr = @selection
-      dest = Dir.pwd
-    elsif Array(src).one?
-      print "Copy to: "
-      dest ||= gets # TODO
+      unless src
+        scr = @selection
+        dest = Dir.pwd
+      elsif Array(src).one?
+        dest ||= @reader.ask("Copy to", src)
+      end
+
+      FileUtils.copy_entry(
+        src, dest, preserve = preserve, dereference_root = dereference_root,
+        remove_destination = remove_destination
+      )
     end
-  
-    FileUtils.copy_file src, dest, preserve = preserve, dereference = dereference
-  end
-  
-  
-  def cp(src, dest, preserve: nil, noop: nil, verbose: nil)
-    unless src
-      scr = @selection
-      dest = Dir.pwd
-    elsif Array(src).one?
-      print "Copy to: "
-      dest ||= gets # TODO
+
+    def copy_file(src, dest = nil, preserve = false, dereference = true)
+      unless src
+        scr = @selection
+        dest = Dir.pwd
+      elsif Array(src).one?
+        dest ||= @reader.ask("Copy to", src)
+      end
+
+      FileUtils.copy_file src, dest, preserve = preserve, dereference = dereference
     end
-  
-    FileUtils.cp src, dest, preserve: preserv, noop: noop, verbose: verbose
-  end
-  
-  def cp_r(
-    src, dest, preserve: nil, noop: nil, verbose: nil,
-    dereference_root: true, remove_destination: nil
-  )
-    unless src
-      scr = @selection
-      dest = Dir.pwd
-    elsif Array(src).one?
-      print "Copy to: "
-      dest ||= gets # TODO
+
+    def cp(src, dest = nil, preserve: nil, noop: nil, verbose: nil)
+      unless src
+        scr = @selection
+        dest = Dir.pwd
+      elsif Array(src).one?
+        dest ||= @reader.ask("Copy to", src)
+      end
+
+      FileUtils.cp src, dest, preserve: preserv, noop: noop, verbose: verbose
     end
-  
-    FileUtils.cp_r(
-      src, dest, preserve: preserv, noop: noop, verbose: verbose,
-      dereference_root: dereference_root,  remove_destination: remove_destination
+
+    def cp_r(
+      src, dest = nil, preserve: nil, noop: nil, verbose: nil,
+      dereference_root: true, remove_destination: nil
     )
-  end
-  
-  
-  def mv(src, dest, force: nil, noop: nil, verbose: nil, secure: nil)
-    unless src
-      scr = @selection
-      dest = Dir.pwd
-    elsif Array(src).one?
-      print "Move to: "
-      dest ||= gets # TODO
+      unless src
+        scr = @selection
+        dest = Dir.pwd
+      elsif Array(src).one?
+        dest ||= @reader.ask("Copy to", src)
+      end
+
+      FileUtils.cp_r(
+        src, dest, preserve: preserv, noop: noop, verbose: verbose,
+        dereference_root: dereference_root,  remove_destination: remove_destination
+      )
     end
-  
-    @selection = FileUtils.mv(
-      src, dest, force: force, noop: noop, verbose: verbose, secure: secure
+
+    def mv(src, dest = nil, force: nil, noop: nil, verbose: nil, secure: nil)
+      unless src
+        scr = @selection
+        dest = Dir.pwd
+      elsif Array(src).one?
+        dest ||= @reader.ask("Move to", src)
+      end
+
+      @selection = FileUtils.mv(
+        src, dest, force: force, noop: noop, verbose: verbose, secure: secure
+      )
+    end
+
+    def link_entry(src, dest = nil, dereference_root = false, remove_destination = false)
+      unless src
+        scr = @selection
+        dest = Dir.pwd
+      elsif Array(src).one?
+        dest ||= @reader.ask("Link to", src)
+      end
+
+      FileUtils.link_entry(
+        src, dest, dereference_root = dereference_root,
+        remove_destination = remove_destination
+      )
+    end
+
+    def ln(targets, link = nil, force: nil, noop: nil, verbose: nil)
+      unless targets
+        targets = @selection
+        link = Dir.pwd
+      elsif Array(src).one?
+        link ||= @reader.ask("Link from", targets)
+      end
+
+      FileUtils.ln targets, link, force: force, noop: noop, verbose: verbose
+    end
+
+    def ln_s(targets, link = nil, force: nil, noop: nil, verbose: nil)
+      unless targets
+        targets = @selection
+        link = Dir.pwd
+      elsif Array(src).one?
+        link ||= @reader.ask("Link from", targets)
+      end
+
+      FileUtils.ln_s targets, link, force: force, noop: noop, verbose: verbose
+    end
+
+    def ln_sf(targets, link = nil, noop: nil, verbose: nil)
+      unless targets
+        targets = @selection
+        link = Dir.pwd
+      elsif Array(src).one?
+        link ||= @reader.ask("Link from", targets)
+      end
+
+      FileUtils.ln_sf targets, link, noop: noop, verbose: verbose
+    end
+
+    def install(
+      src, dest = nil, mode: nil, owner: nil, group: nil,
+      preserve: nil, noop: nil, verbose: nil
     )
-  end
-  
-  
-  def link_entry(src, dest, dereference_root = false, remove_destination = false)
-    unless src
-      scr = @selection
-      dest = Dir.pwd
-    elsif Array(src).one?
-      print "Move to: "
-      dest ||= gets # TODO
+      unless src
+        scr = @selection
+        dest = Dir.pwd
+      elsif Array(src).one?
+        dest ||= @reader.ask("Install to", src)
+      end
+
+      FileUtils.install(
+        src, dest, mode: mode, owner: owner, group: group,
+        preserve: preserve, noop: noop, verbose: verbose
+      )
     end
-  
-    FileUtils.link_entry(
-      src, dest, dereference_root = dereference_root,
-      remove_destination = remove_destination
-    )
-  end
-  
-  
-  def ln(targets, link, force: nil, noop: nil, verbose: nil)
-    unless targets
-      targets = @selection
-      link = Dir.pwd
-    elsif Array(src).one?
-      print "Link from: "
-      link ||= gets # TODO
-    end
-  
-    FileUtils.ln targets, link, force: force, noop: noop, verbose: verbose
-  end
-  
-  def ln_s(targets, dir, force: nil, noop: nil, verbose: nil)
-    unless targets
-      targets = @selection
-      link = Dir.pwd
-    elsif Array(src).one?
-      print "Link from: "
-      link ||= gets # TODO
-    end
-  
-    FileUtils.ln_s targets, link, force: force, noop: noop, verbose: verbose
-  end
-  
-  def ln_sf(targets, dir, noop: nil, verbose: nil)
-    unless targets
-      targets = @selection
-      link = Dir.pwd
-    elsif Array(src).one?
-      print "Link from: "
-      link ||= gets # TODO
-    end
-  
-    FileUtils.ln_sf targets, link, noop: noop, verbose: verbose
-  end
-  
-  
-  def install(
-    src, dest, mode: nil, owner: nil, group: nil, preserve: nil, noop: nil, verbose: nil
-  )
-    unless src
-      scr = @selection
-      dest = Dir.pwd
-    elsif Array(src).one?
-      print "Install to: "
-      dest ||= gets # TODO
-    end
-  
-    FileUtils.mv(
-      src, dest, mode: mode, owner: owner, group: group, preserve: preserve, noop: noop, verbose: verbose
-    )
   end
 end
